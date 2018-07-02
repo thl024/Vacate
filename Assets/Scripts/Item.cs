2fa48625d5c421c5335ea825dfd2075ca19afa39
thl024 <lamtimothy1996@gmail.com>
2018-07-02 15:22:50 -0700

    Initial Item class

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teddyBear : MonoBehaviour {
+public class Item : MonoBehaviour {
+
+	public string name;
+	public string description;
 
 	// Use this for initialization
 	void Start () {
