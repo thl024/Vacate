345142ce2e0911e90dad7500f5b55d3cf50b62eb
thl024 <lamtimothy1996@gmail.com>
2018-07-07 11:36:32 -0700

    Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public List<Item> items;
+
+	// Use this for initialization
+	void Start () {
+		items = new List<Item>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddItem() {
+		// TODO
+	}
+
+	public void hasItem() {
+		// TODO
+	}
+}
